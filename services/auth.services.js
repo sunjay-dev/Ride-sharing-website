5ea8d31ca135089eb67ad228b048a368fb409bb1
Sunjay Kumar <ak2604745@gmail.com>
2025-03-02 11:59:01 +0500

    JWT_Secret to JWT_SECRET

--- services/auth.services.js
+++ services/auth.services.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const secret = process.env.JWT_Secret;
+const secret = process.env.JWT_SECRET;
 
 function setUser(id) {
     return jwt.sign({
