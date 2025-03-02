11a4a879e723249c49b6ce8f720227e009bd501d
Sunjay Kumar <ak2604745@gmail.com>
2025-03-02 12:04:23 +0500

    try catch block in forget password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -135,79 +135,85 @@ module.exports.sendAllMessages = async (req, res, next) => {
 
 module.exports.forgetPassword = async (req, res, next) => {
     const { email } = req.body;
-    const user = await userModel.findOne({ email: email.toLowerCase() });
-
-    if (!user) return res.status(404).json({
-        message: "No User Found."
-    });
-
-    if (user.resetTokenExpiry && user.resetTokenExpiry > Date.now()) {
-        return res.status(429).json({ message: "You can request a reset link only once per hour." });
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-
-    // Create reset link
-    const resetLink = `${process.env.CLIENT_URL}/reset-password/${token}`;
-
-    sendemail(resetLink, email, user.firstName, user.lastName);
-
-    user.resetToken = token;
-    user.resetTokenExpiry = Date.now() + 3600000;
-    await user.save();
-
-    return res.status(200).json({
-        message: "Email sent to reset password"
-    });
-}
-
-module.exports.resetPassword = async (req, res, next) => {
-
-    const { token, password } = req.body;
-    if (!token || !password) return res.status(400).json({
-        message: "Token and password are required"
-    });
-
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await userModel.findOne({ _id: decoded.userId });
+        const user = await userModel.findOne({ email: email.toLowerCase() });
 
         if (!user) return res.status(404).json({
             message: "No User Found."
         });
 
-        const hashedPassword = await userModel.hashPassword(password);
-        user.password = hashedPassword;
-        user.resetToken = undefined;
-        user.resetTokenExpiry = undefined
+        if (user.resetTokenExpiry && user.resetTokenExpiry > Date.now()) {
+            return res.status(429).json({ message: "You can request a reset link only once per hour." });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        // Create reset link
+        const resetLink = `${process.env.CLIENT_URL}/reset-password/${token}`;
+
+        sendemail(resetLink, email, user.firstName, user.lastName);
+
+        user.resetToken = token;
+        user.resetTokenExpiry = Date.now() + 3600000;
         await user.save();
 
-        const Token_cookie = setUser(user._id);
-        res.cookie('token', Token_cookie);
-
         return res.status(200).json({
-            message: "Password reset successfully"
+            message: "Email sent to reset password"
         });
     } catch (error) {
-        return res.status(400).json({
-            message: "Invalid or expired token"
+        return res.status(404).json({
+            message: "Error sending email",
+            error : error
         });
     }
 }
+    module.exports.resetPassword = async (req, res, next) => {
 
+        const { token, password } = req.body;
+        if (!token || !password) return res.status(400).json({
+            message: "Token and password are required"
+        });
 
-module.exports.getUserRideStats = async (req, res, next) => {
-    const userId = req.user.id;
-    try {
-        const [ridesCreated, ridesCompleted, ridesCanceled] = await Promise.all([
-            rideModel.countDocuments({ driver: userId }),
-            rideModel.countDocuments({ driver: userId, status: "completed" }),
-            rideModel.countDocuments({ driver: userId, status: "canceled" })
-        ]);
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const user = await userModel.findOne({ _id: decoded.userId });
 
-        res.status(200).json({ ridesCreated, ridesCompleted, ridesCanceled });
-    } catch (error) {
-        console.error("Error fetching ride stats:", error);
-        return { ridesCreated: 0, ridesCompleted: 0, ridesCanceled: 0 };
+            if (!user) return res.status(404).json({
+                message: "No User Found."
+            });
+
+            const hashedPassword = await userModel.hashPassword(password);
+            user.password = hashedPassword;
+            user.resetToken = undefined;
+            user.resetTokenExpiry = undefined
+            await user.save();
+
+            const Token_cookie = setUser(user._id);
+            res.cookie('token', Token_cookie);
+
+            return res.status(200).json({
+                message: "Password reset successfully"
+            });
+        } catch (error) {
+            return res.status(400).json({
+                message: "Invalid or expired token"
+            });
+        }
     }
-};
+
+
+    module.exports.getUserRideStats = async (req, res, next) => {
+        const userId = req.user.id;
+        try {
+            const [ridesCreated, ridesCompleted, ridesCanceled] = await Promise.all([
+                rideModel.countDocuments({ driver: userId }),
+                rideModel.countDocuments({ driver: userId, status: "completed" }),
+                rideModel.countDocuments({ driver: userId, status: "canceled" })
+            ]);
+
+            res.status(200).json({ ridesCreated, ridesCompleted, ridesCanceled });
+        } catch (error) {
+            console.error("Error fetching ride stats:", error);
+            return { ridesCreated: 0, ridesCompleted: 0, ridesCanceled: 0 };
+        }
+    };
